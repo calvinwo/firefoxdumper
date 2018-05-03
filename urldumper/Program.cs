dc66fac463bdf6f59f87c5e581d2053d56e64b04
calvinwo <calvincdw@gmail.com>
2018-05-02 22:35:13 -1000

    Initial commit

---
+++ urldumper/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using IniParser;
+
+namespace urldumper
+{
+    class Options
+    {
+        [Option('i', "input", Required = false, HelpText = "Input file to be processed.")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Output file.")]
+        public string OutputFile { get; set; }
+
+        /*[Option('f', "format", Required = false, HelpText = "Format of the output file.")]
+        public string Format { get; set; }*/
+
+        // Omitting long name, defaults to name of property, ie "--verbose"
+        [Option(Default = false, HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+                .WithNotParsed<Options>((errs) => HandleParseError(errs));
+        }
+
+        static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            var input = opts.InputFile;
+            var output = opts.OutputFile;
+            if (String.IsNullOrEmpty(input))
+            {
+                string firefox = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Mozilla\Firefox";
+                string profileini = $"{firefox}\\profiles.ini";
+                var inidata = new FileIniDataParser().ReadFile(profileini);
+                var profile = "";
+
+                try
+                {
+                    profile = inidata["Profile0"]["Path"];
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+                    throw;
+                }
+                profile = $"{firefox}\\{profile}";
+                input = $"{profile}\\sessionstore-backups\\recovery.jsonlz4";
+            }
+            if (String.IsNullOrEmpty(output))
+            {
+                output = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\links\\firefox{DateTime.Today.ToString("MMddyyyy")}.html";
+            }
+            new UrlDumper().Dump(input, output);
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            foreach(var err in errs)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+    }
+}
---
+++ urldumper/UrlDumper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace urldumper
+{
+    class UrlDumper
+    {
+        public void Dump(string input, string output)
+        {
+            string tempfile = System.IO.Path.GetTempPath() + "recovery.json";
+            try
+            {
+                DecompressJson(input, tempfile);
+                DumpJson(tempfile, output);
+            }
+            finally
+            {
+                if(File.Exists(tempfile))
+                {
+                    File.Delete(tempfile);
+                }
+            }
+        }
+
+        static void DecompressJson(string input, string output)
+        {
+            var decompressor = System.IO.Path.GetTempPath() + "decompressor.exe";
+
+            try
+            {
+                File.WriteAllBytes(decompressor, Properties.Resources.dejsonlz4);
+                // Use ProcessStartInfo class
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    CreateNoWindow = false,
+                    UseShellExecute = false,
+                    FileName = decompressor,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    Arguments = $"{input} {output}"
+                };
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+            }
+            catch (Exception e)
+            {
+                // Log error.
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if(File.Exists(decompressor))
+                {
+                    File.Delete(decompressor);
+                }
+            }
+        }
+
+        static void DumpJson(string input, string output)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (var fileStream = new FileStream(input, FileMode.Open))
+                using (var reader = new StreamReader(fileStream))
+                using (var file = new StreamWriter(output))
+                {
+                    string line = reader.ReadToEnd();
+                    var json = JObject.Parse(line);
+                    foreach(var w in json.SelectToken("windows").Children())
+                    {
+                        foreach(var t in w.SelectToken("tabs").Children())
+                        { 
+                            try
+                            {
+                                JArray arr = (JArray)t.First.First;
+                                foreach (var jtoken in arr)
+                                {
+                                    var link = jtoken.SelectToken("url").ToString();
+                                    var title = jtoken.SelectToken("title").ToString();
+                                    file.WriteLine($"<a href=\"{link}\">{title}</a><br />");
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
